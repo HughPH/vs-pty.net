bb63e711fb539c308a8c1d71e0d2534793388398
Hugh Phoenix-Hulme <hugh@webnet.org.uk>
2022-12-22 20:19:03 +0000

    Added working dotnet6 console app and failing dotnet7 console app Updated all the nuget packages Updated Pty.Net.Tests to net6.0 Removed Nerdbank because it does stuff I'm not interested in

---
+++ src/dotnet6/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using Pty.Net;
+
+Console.WriteLine("Create Options");
+PtyOptions options = new PtyOptions
+                     {
+                         App = "/bin/bash",
+                         Cwd = Environment.CurrentDirectory
+                     };
+Console.WriteLine("SpawnAsync");
+IPtyConnection connection = await PtyProvider.SpawnAsync(options, CancellationToken.None);
+Console.WriteLine("Write 'exit\\n' to stdin");
+connection.WriterStream.Write(Encoding.UTF8.GetBytes("exit\n"));
+Console.WriteLine("Flush stdin");
+connection.WriterStream.Flush();
+Console.WriteLine("Wait for exit");
+connection.WaitForExit(100);
+Console.WriteLine("Done");
